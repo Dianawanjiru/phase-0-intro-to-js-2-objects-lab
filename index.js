bdd601223dba6b3ca85cac5950c0cd6bb1c21592
Diana <diana.wanjiru@student.moringaschool.com>
2022-08-20 17:10:41 +0300

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Diana",
+   streetAdress : "12 Philadelphia",
+}
+
+function updateEmployeeWithKeyAndValue (employee, key,value){
+    return {...employee,[key]: value,};
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key,value){
+    employee [key]= value;
+    return employee;
+}
+ 
+function deleteFromEmployeeByKey (employee,key){
+     
+    let newWord = {...employee};
+    delete newWord[key];
+    return newWord;
+}
+function destructivelyDeleteFromEmployeeByKey (employee, key, value){
+    
+    let newObj = employee;
+    delete newObj[key];
+    return newObj;
+
+}
